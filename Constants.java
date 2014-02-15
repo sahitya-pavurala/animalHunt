aa6d2a813ccb333759d7da40fa4a30ee2b7bc41e
sahitya-pavurala <sahitya.pavurala@yahoo.com>
2014-02-14 20:09:09 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,7 @@
+package animalHunt;
+
+public final class Constants {
+	public static final String animals[] = {"A1","A2","A3","A4","A5"};
+	public static final String weapons[] = {"W1","W2","W3","W4","W5"};
+	public static final int count = 5;
+}
